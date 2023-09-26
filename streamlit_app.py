ddff0cf22c520c7f4e4354db9b341c87c795b9fe
shouda-tomoki <146058298+shouda-tomoki@users.noreply.github.com>
2023-09-26 06:31:41 +0000

    えとぇｇ

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,13 @@ import random
 st.set_page_config(page_title="タイトル", layout="wide")
 
 # タイトルを設定
-st.title('いろいろ練習中')
+st.title('☆いろいろ練習中☆')
 
 # テキスト入力ボックスを作成し、ユーザーからの入力を受け取る
 user_input = st.text_input('あなたの名前を入力してください')
 
 # ボタンを作成し、クリックされたらメッセージを表示
-if st.button('挨拶する'):
+if st.button('挨拶をしましょう'):
     if user_input:  # 名前が入力されているかチェック
         st.success(f'🌟 おはよう、{user_input}さま! 🌟')  # メッセージをハイライト
     else:
@@ -25,7 +25,7 @@ number = st.slider('好きな数字（10進数）を選んでください', 0, 1
 st.caption("十字キー（左右）でも調整できます。")
 
 # 選択した数字を表示
-st.write(f'あなたが選んだ数字は「{number}」です。')
+st.write(f'あなたが選んだ数字は...「{number}」です。')
 
 # 選択した数値を2進数に変換
 binary_representation = bin(number)[2:]  # 'bin'関数で2進数に変換し、先頭の'0b'を取り除く
